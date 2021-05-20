a41ef238fc46c122380d4548ca4d15bcc3b72b6e
anzboi <daniel.cantos@anz.com>
2021-05-20 13:05:19 +1000

    attempt use https_proxy

--- src/octokit-client.ts
+++ src/octokit-client.ts
@@ -1,7 +1,18 @@
 import {Octokit as Core} from '@octokit/core'
 import {paginateRest} from '@octokit/plugin-paginate-rest'
 import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods'
+import {HttpsProxyAgent} from 'https-proxy-agent'
 export {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods'
 export {OctokitOptions} from '@octokit/core/dist-types/types'
 
-export const Octokit = Core.plugin(paginateRest, restEndpointMethods)
+let agent = {};
+var proxy = process.env.https_proxy || process.env.HTTPS_PROXY;
+if (proxy) {
+    agent = new HttpsProxyAgent(proxy)
+}
+
+// export const Octokit = Core.plugin(paginateRest, restEndpointMethods)
+export const Octokit = Core.defaults({
+    request: { agent: agent }
+}).plugin(paginateRest, restEndpointMethods)
+// }paginateRest, restEndpointMethods)
